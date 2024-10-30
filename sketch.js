3983602e2a2d8bfad79c03c33582bdcbb6ef1faf
Hannah Hansted <hmh22@fsu.edu>
2024-10-30 10:34:00 -0400

    blockout setup

--- sketch.js
+++ sketch.js
@@ -1,18 +1,60 @@
-let ball;
+let mirror, phone;
+let tanktop;
+let shirt;
+let sweatshirt;
 
 function setup() {
-	new Canvas(500, 500);
+	new Canvas(windowWidth, windowHeight);
 	displayMode('centered');
 
-	ball = new Sprite();
-	ball.diameter = 50;
+	mirror = new Sprite(300, 350, 300, 500, 's');
+	mirror.text = "mirror";
+
+	phone = new Sprite (1260, 350, 300, 500, 's');
+	phone.text = "phone";
+
+	tanktop = new Sprite(650, 350, 150, 250, 'k');
+	tanktop.text = "tanktop";
+	tanktop.drag = 8;
+
+	shirt = new Sprite(780, 350, 150, 250, 'k');
+	shirt.text = "shirt";
+	shirt.drag = 8;
+
+	sweatshirt = new Sprite(920, 350, 150, 250, 'k');
+	sweatshirt.text = "sweatshirt";
+	sweatshirt.drag = 8;
+
 }
 
 function draw() {
-	background('skyblue');
+	background('#ecf7ba');
 
-	if (mouse.presses()) {
-		ball.speed = 10;
-		ball.moveTo(mouse);
+	if (tanktop.mouse.dragging()){
+		tanktop.moveTowards(
+			mouseX + tanktop.mouse.x,
+			mouseY + tanktop.mouse.y,
+		1);
+	} else {
+		tanktop.moveTowards(600, 350, .04);
 	}
+
+	if (shirt.mouse.dragging()){
+		shirt.moveTowards(
+			mouseX + shirt.mouse.x,
+			mouseY + shirt.mouse.y,
+		1);
+	} else {
+		shirt.moveTowards(780, 350, .04);
+	}
+
+	if (sweatshirt.mouse.dragging()){
+		sweatshirt.moveTowards(
+			mouseX + sweatshirt.mouse.x,
+			mouseY + sweatshirt.mouse.y,
+		1);
+	} else {
+		sweatshirt.moveTowards(960, 350, .04);
+	}
+
 }
